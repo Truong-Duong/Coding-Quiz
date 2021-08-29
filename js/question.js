5adf449e73799a9115b49c7f06a094e00fa71284
Truong Duong <truong.duong1908@gmail.com>
2021-08-29 00:39:17 -0500

    final product

---
+++ js/question.js
@@ -0,0 +1,61 @@
+var questionsArray = [
+    {
+        title: "Question 1: Which of the following is true about variable naming conventions in JavaScript?",
+        options: 
+        [
+            "A. JavaScript variable names must begin with a letter or the underscore character.", 
+            "B. JavaScript variable names are case sensitive.", 
+            "C. Both of the above.", 
+            "D. None of the above.", 
+        ],
+        answer: "C. Both of the above.",
+    },
+
+    {
+        title: "Question 2: Which of the following is true about cookie handling in JavaScript?",
+        options: 
+        [
+            "A. JavaScript can manipulate cookies using the cookie property of the Document object.",
+            "B. JavaScript can read, create, modify, and delete the cookie or cookies that apply to the current web page.",
+            "C. Both of the above.",
+            "D. None of the above.",
+        ],
+        answer: "C. Both of the above.",
+    },
+
+    {
+        title: "Question 3: Which of the following type of variable takes precedence over other if names are same?",
+        options: 
+        [
+            "A. global variable",
+            "B. local variable",
+            "C. Both of the above.",
+            "D. None of the above.",
+        ],
+        answer: "B. local variable",
+    },
+
+    {
+        title: "Question 4: Which of the following function of Number object forces a number to display in exponential notation?",
+        options: 
+        [
+            "A. toExponential()",
+            "B. toFixed()",
+            "C. toPrecision()",
+            "D. toLocaleString()",
+        ],
+        answer: "A. toExponential()",
+    },
+
+    {
+        title: "Question 5: Which of the following function of Boolean object returns the primitive value of the Boolean object?",
+        options: 
+        [
+            "A. toSource()",
+            "B. valueOf()",
+            "C. toString()",
+            "D. None of the above.",
+        ],
+        answer: "B. valueOf()",
+    },
+  ];
